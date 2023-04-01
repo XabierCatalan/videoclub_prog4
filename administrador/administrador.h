e854102735e9d40cd35bd4a9d1401152b84df72c
XabierCatalan <xabier.catalan@opendeusto.es>
2023-04-01 18:37:41 +0200

    creacion estructura administrador

---
+++ administrador/administrador.h
@@ -0,0 +1,24 @@
+/*
+ * administrador.h
+ *
+ *  Created on: 1 abr 2023
+ *      Author: xabic
+ */
+
+#ifndef ADMINISTRADOR_ADMINISTRADOR_H_
+#define ADMINISTRADOR_ADMINISTRADOR_H_
+
+typedef struct {
+	int id_admin;
+	char *nombre_admin;
+	char *gmail_admin;
+	char *contra_admin;
+
+
+}Administrador;
+
+
+
+
+
+#endif /* ADMINISTRADOR_ADMINISTRADOR_H_ */
