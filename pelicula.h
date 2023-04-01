62668b5f3dd0e82bed54c83db55a8114d0b0b82b
Oscar Perez <o.perez@opendeusto.es>
2023-04-01 14:08:54 +0200

    estructura peliculas

---
+++ pelicula.h
@@ -0,0 +1,23 @@
+/*
+ * pelicula.h
+ *
+ *  Created on: 1 abr 2023
+ *      Author: oscar
+ */
+
+#ifndef PELICULA_H_
+#define PELICULA_H_
+
+typedef struct {
+	int id_pelicula;
+	char *nombre;
+	int cod_genero;  //crear una tabla en la base de datos con cod_genero y genero
+	char *director;
+	int cod_formato;  //crear una tabla en la base de datos con cod_formato y formato
+	char *fecha_sal;
+	float precio;
+
+}Pelicula;
+
+
+#endif /* PELICULA_H_ */
