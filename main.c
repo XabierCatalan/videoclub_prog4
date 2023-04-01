c4eadd6df5843ef4d45024d521a2e13347416cb9
Oscar Perez <o.perez@opendeusto.es>
2023-04-01 17:49:46 +0200

    prototico menu

--- main.c
+++ main.c
@@ -1,11 +1,43 @@
-
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main() {
+    int opcion;
 
-	printf("hola");
+    do {
+        printf("\n\n----Menu de opciones----\n\n");
+        printf("1. Agregar una película\n");
+        printf("2. Eliminar una película\n");
+        printf("3. Mostrar todas las películas\n");
+        printf("4. Salir del programa\n");
+        printf("\nIngrese una opción: ");
+        scanf("%d", &opcion);
 
+        if (opcion == 1) {
 
-	return 0;
+                printf("\nHa seleccionado la opción 1: Agregar una película\n");
+                // Aquí iría el código para agregar una película
+                break;
+        } else if (opcion == 2) {
+
+                printf("\nHa seleccionado la opción 2: Eliminar una película\n");
+                // Aquí iría el código para eliminar una película
+                break;
+        } else if (opcion == 3){
+
+            case 3:
+                printf("\nHa seleccionado la opción 3: Mostrar todas las películas\n");
+                // Aquí iría el código para mostrar todas las películas
+                break;
+        } else if (opcion == 4) {
+
+                printf("\nHa seleccionado la opción 4: Salir del programa\n");
+                break;
+           // default:
+           // printf("\nOpción no válida. Intente de nuevo.\n");
+           // break;
+        }
+    } while(opcion != 4);
+
+    return 0;
 }
