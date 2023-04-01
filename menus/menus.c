04f25c7f4e63e905b1d8b8853b450b43cb91e131
Oscar Perez <o.perez@opendeusto.es>
2023-04-01 19:40:34 +0200

    cases

--- menus/menus.c
+++ menus/menus.c
@@ -23,26 +23,40 @@ void menuPrincipal(){
 		printf("Introduce una opcion del menu: \n");
 		fflush(stdout);
 		scanf("%i", &opcion);
-		if ( opcion == 1 ){
+
+		switch(opcion) {
+			case 1:
+
 			printf("La opcion elegida es 1.");
-			//break;
-		} else if( opcion == 2 ){
+			break;
+
+			case 2:
+
 			printf("La opcion elegida es 2.");
-			//break;
-		} else if( opcion == 3 ){
+			break;
+
+			case 3:
+
 			printf("La opcion elegida es 3.");
-			//break;
-		}else if( opcion == 4 ){
+			break;
+
+			case 4:
+
 			printf("La opcion elegida es 4.");
-			//break;
-		}else if( opcion == 5 ){
+			break;
+
+			case 5:
+
 			printf("La opcion elegida es 5.");
-			//break;
-		}else{
+			break;
+
+			default:
+
 			printf("SELECCIONA UNA OPCION VALIDA.\n");
-			//break;
+			break;
+
 		}
 
-	}while( opcion =! 5);
+	} while( opcion =! 5);
 
 }
