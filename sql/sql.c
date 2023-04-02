dd03969df2727e3491af88f24e4a17328f6f0639
XabierCatalan <xabier.catalan@opendeusto.es>
2023-04-02 11:56:16 +0200

    pruba en sql.c

--- sql/sql.c
+++ sql/sql.c
@@ -6,18 +6,37 @@
  */
 
 #include "sqlite3.h"
+#include <string.h>
+#include <stdio.h>
 
-void inicializar()
-{
+
 	sqlite3 *db;
 	sqlite3_stmt *stmt;
 	int result;
 
+void inicializar()
+{
 	sqlite3_open("BDD_Prog.db", &db);
 }
 
 void cargarPeliculas()
 {
+	char sql2[] = "select Titulo_Pelicula from Peliculas";
+
+	sqlite3_prepare_v2(db, sql2, strlen(sql2), &stmt, NULL) ;
+
+	printf("\n");
+	printf("Mostrando peliculas:\n");
+
+	do {
+			result = sqlite3_step(stmt) ;
+			if (result == SQLITE_ROW) {
+				printf("%s\n", (char*) sqlite3_column_text(stmt, 0));
+			}
+		} while (result == SQLITE_ROW);
+		printf("\n");
+
+		sqlite3_finalize(stmt);
 
 }
 
