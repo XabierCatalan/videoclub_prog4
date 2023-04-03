554370c4c8e904698049a34dededf9e9a067aa0d
Usuario <Usuario@DESKTOP-JL6618N>
2023-04-03 20:01:24 +0200

    aaaaaaaaaaa

--- logger/logger.c
+++ logger/logger.c
@@ -7,12 +7,12 @@
 #include <stdio.h>
 
 
-void mensaje_log(char *mensaje, char *archivo) {
-    FILE *log_file = fopen(archivo, "a"); // abre el archivo en modo de escritura (append)
-    if (log_file == NULL) {
-        printf("No se pudo abrir el archivo de registro.\n");
-        return;
-    }
-    fprintf(log_file, "Error: %s\n", mensaje); // escribe el mensaje de error en el archivo
-    fclose(log_file); // cierra el archivo
-}
+//void mensaje_log(char *mensaje, char *archivo) {
+//    FILE *log_file = fopen(archivo, "a"); // abre el archivo en modo de escritura (append)
+//    if (log_file == NULL) {
+//        printf("No se pudo abrir el archivo de registro.\n");
+//        return;
+//    }
+//    fprintf(log_file, "Error: %s\n", mensaje); // escribe el mensaje de error en el archivo
+//    fclose(log_file); // cierra el archivo
+//}
