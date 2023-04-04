c11023427e30558a564d4a85bd53dba8933946fe
Oscar Perez <o.perez@opendeusto.es>
2023-04-04 19:15:48 +0200

    p

--- main.c
+++ main.c
@@ -9,22 +9,6 @@
 
 int main(void) {
 	menuInicioSesion();
-	//menuPrincipal();
-
-//	Pelicula *p;
-//	p = (Pelicula*) malloc(sizeof(Pelicula)*2);
-//
-//	Pelicula peli = {1,"transformers", 3, "koldo", 4, "fecha", 34.02};
-//	Pelicula peli2 = {2,"transformers2", 1, "oscar", 3, "fecha1", 24.02};
-//
-//	p[0] = peli;
-//	p[1] = peli2;
-//
-// 	imprimirPeliculas(p, 2);
-
-//	int num_pelis = contarPeliculas();
-//	printf("%i", num_pelis);
-//	return 0;
 
 }
 
